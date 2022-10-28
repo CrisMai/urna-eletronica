67d367f9136e72d187c15976ef567de6afb7de11
CrisMai <crismai.dev@gmail.com>
2022-10-28 15:52:00 -0300

    Etapa criação dos candidatos

---
+++ etapas.js
@@ -0,0 +1,54 @@
+
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+        {
+            numero: 85551,
+            nome: 'Fulano de Tal',
+            partido: 'ABC',
+            fotos:[
+            {url:'85551.png', legenda: 'Vereador'}
+            ]
+        },
+        {
+            numero: 95554,
+            nome: 'BELTRANO DA SILVA',
+            partido: 'DFG',
+            fotos:[
+            {url:'95554.png', legenda: 'Vereador'}
+            ]
+        },
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 80,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: '80.png', legenda: 'Prefeito'},
+                    {url:'80_2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 81,
+                nome: 'Zulano',
+                partido: 'DEFG',
+                vice: 'Zul',
+                fotos: [
+                    {url: '81.png', legenda:'Prefeito'},
+                    {url: '81_2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        
+        ]
+    }
+
+];
